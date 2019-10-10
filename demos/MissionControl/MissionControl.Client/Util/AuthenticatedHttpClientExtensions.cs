76f80a845a29c95d467bf221bddcdfe5d8258fd9
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-10-10 17:48:49 +0100

    Minor code tidying

---
+++ demos/MissionControl/MissionControl.Client/Util/AuthenticatedHttpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MissionControl.Client.Util
+{
+    public static class AuthenticatedHttpClientExtensions
+    {
+        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url, AuthenticationHeaderValue authorization)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = authorization;
+
+            var response = await httpClient.SendAsync(request);
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+        }
+    }
+}
--- demos/MissionControl/MissionControl.Client/Util/ServiceExtensions.cs
+++ demos/MissionControl/MissionControl.Client/Util/ServiceExtensions.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Security.Claims;
-using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,16 +17,6 @@ namespace MissionControl.Client.Util
             services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<TokenAuthenticationStateProvider>());
         }
 
-        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url, AuthenticationHeaderValue authorization)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Authorization = authorization;
-
-            var response = await httpClient.SendAsync(request);
-            var responseBytes = await response.Content.ReadAsByteArrayAsync();
-            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-        }
-
         public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
         {
             var payload = jwt.Split('.')[1];
@@ -41,7 +25,7 @@ namespace MissionControl.Client.Util
             return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
         }
 
-        static byte[] ParseBase64WithoutPadding(string base64)
+        private static byte[] ParseBase64WithoutPadding(string base64)
         {
             switch (base64.Length % 4)
             {
